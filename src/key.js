937126a7acc4b8fefc95a6fd38627bc9d53e8f0d
empollica <rolimat@gmail.com>
2018-03-13 20:25:33 +0100

    Hide api key

---
+++ src/key.js
@@ -0,0 +1,1 @@
+module.exports = 'paste_your_api_key_here';
--- src/utils/filmsRequest.js
+++ src/utils/filmsRequest.js
@@ -1,8 +1,9 @@
 var ajax = require('@fdaciuk/ajax');
 var app = require('ampersand-app');
+var apiKey = require('../key');
 
 var getFilms = function(page, collection) {
-	ajax().get('https://api.themoviedb.org/3/discover/movie', {'api_key': 'c5e4a1733c2995102fafe209c014e4c0', 'page': page}).then(function (response, xhr) {
+	ajax().get('https://api.themoviedb.org/3/discover/movie', {'api_key': apiKey, 'page': page}).then(function (response, xhr) {
   		collection.addFilmsFromResponse(response.results);
 	});
 };
